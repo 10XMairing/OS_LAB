4ecf71c6f33dbb0e2700c82347642c8befa2e20b
10xmairing <mairingdao@gmail.com>
2020-01-30 15:33:30 +0530

    2nd day

---
+++ 30_01_2020/stat.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <time.h>
+
+
+void printFileProperties(struct stat stats);
+
+
+int main()
+{
+    char path[100];
+    struct stat stats;
+
+    printf("Enter source file path: ");
+    scanf("%s", path);
+
+
+    // stat() returns 0 on successful operation,
+    // otherwise returns -1 if unable to get file properties.
+    if (stat(path, &stats) == 0)
+    {
+        printFileProperties(stats);
+    }
+    else
+    {
+        printf("Unable to get file properties.\n");
+        printf("Please check whether '%s' file exists.\n", path);
+    }
+
+    return 0;
+}
+
+
+
+/**
+ * Function to print file properties.
+ */
+void printFileProperties(struct stat stats)
+{
+    struct tm dt;
+
+    // File permissions
+    printf("\nFile access: ");
+    if (stats.st_mode & R_OK)
+        printf(" read");
+    if (stats.st_mode & W_OK)
+        printf(" write");
+    if (stats.st_mode & X_OK)
+        printf(" execute");
+
+    // File size
+    printf("\nFile size: %d", stats.st_size);
+    printf("\nDevice ID : %d" , stats.st_dev);
+    printf("\nINode no : %d" , stats.st_ino);
+    printf("\nBlocks : %d" , stats.st_blocks);
+    printf("\nBlock Size : %d" , stats.st_blksize);
+
+
+    // Get file creation time in seconds and 
+    // convert seconds to date and time format
+    dt = *(gmtime(&stats.st_ctime));
+    printf("\nCreated on: %d-%d-%d %d:%d:%d\n", dt.tm_mday, dt.tm_mon, dt.tm_year + 1900, 
+                                              dt.tm_hour, dt.tm_min, dt.tm_sec);
+
+    // File modification time
+    dt = *(gmtime(&stats.st_mtime));
+    printf("\nModified on: %d-%d-%d %d:%d:%d\n", dt.tm_mday, dt.tm_mon, dt.tm_year + 1900, 
+                                              dt.tm_hour, dt.tm_min, dt.tm_sec);
+
+}
