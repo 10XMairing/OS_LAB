1cc40ce0a0dc4aa5665fc40ddf572a6d1e206c76
10xmairing <10xpumba@gmail.com>
2020-01-30 13:30:27 +0530

    added 3 c files abc

---
+++ 24_01_2020/Qa.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main()
+{
+
+    // make two process which run same
+    // program after this instruction
+    int status = fork(); //returns 0 when a a child process is created by parent &&& returns parentid when called by child id;
+
+    if (status == 0)
+    {
+        // child process
+        printf("child : pid() : %d \n", getpid());
+        printf("child ppid() : %d \n", getppid());
+
+        // char * argv_list[] = {"ls","-lart","/home/mairingdao/Documents" ,NULL};
+        // execv("/bin/ls",argv_list);
+        // execl("/bin/ls" , "ls", "-l" , NULL);
+        // exit(0);
+    }
+    else if (status > 0)
+    {
+        printf("parent pid() : %d \n", getpid());
+        printf("parent ppid() : %d \n", getppid());
+    }
+    else
+    {
+        printf("Error creating child process\n");
+    }
+    printf("Hello world!\n");
+    return 0;
+}
